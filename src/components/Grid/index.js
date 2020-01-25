e2fc5503b8d625c71bcc07c8e59fcbc97eb9807a
SirPotatoIV <otoolej33@gmail.com>
2020-01-25 16:13:41 -0600

    Added first components and tested out the component grid to make sure it was connecting correctly.

---
+++ src/components/Grid/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.css"
+
+function Grid({gridSize}){
+
+    return(
+        <div className="Grid">
+            This is a test
+        </div>
+    )
+}
+export default Grid;
