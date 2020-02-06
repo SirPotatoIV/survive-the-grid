a8795cf7fd0a84105ad51e7851f691aaa039489a
SirPotatoIV <otoolej33@gmail.com>
2020-02-06 15:42:17 -0600

    Can now shoot in all directions.

--- src/utils/shootProjectile.js
+++ src/utils/shootProjectile.js
@@ -1,15 +1,49 @@
 export default function shootProjectile(state){
     //detect orientation
     if(state.player.position.orientation === 0){
-        console.log("shot north")
         
         const newProjectile = {
             player: state.player.name,
             distanceTraveled: 0,
-            shouldExist: true,
             x: state.player.position.x,
             y: state.player.position.y -1,
-            orientation: 0
+            orientation: state.player.position.orientation
+        }
+        return(newProjectile)
+    }
+
+    if(state.player.position.orientation === 180){
+       
+        const newProjectile = {
+            player: state.player.name,
+            distanceTraveled: 0,
+            x: state.player.position.x,
+            y: state.player.position.y + 1,
+            orientation: state.player.position.orientation
+        }
+        return(newProjectile)
+    }
+
+    if(state.player.position.orientation === 90){
+        
+        const newProjectile = {
+            player: state.player.name,
+            distanceTraveled: 0,
+            x: state.player.position.x + 1,
+            y: state.player.position.y,
+            orientation: state.player.position.orientation
+        }
+        return(newProjectile)
+    }
+
+    if(state.player.position.orientation === 270){
+        
+        const newProjectile = {
+            player: state.player.name,
+            distanceTraveled: 0,
+            x: state.player.position.x - 1,
+            y: state.player.position.y,
+            orientation: state.player.position.orientation
         }
         return(newProjectile)
     }
