1b5d89a3c286838fbb6146e9da56c7581fddf42d
SirPotatoIV <otoolej33@gmail.com>
2020-02-09 11:35:30 -0600

    Added lots of comments. Started creating a new component to indicate the end of the game and the winner.

--- src/hooks/useGameLoop.js
+++ src/hooks/useGameLoop.js
@@ -32,14 +32,15 @@ export default function useGameLoop(state, dispatch){
                     payload: {newState}
                 })
             }
+            // checks to see how many players are remaining.
+            // -- if there is only one player remaining, the game is over
             if(Object.entries(newState.players).length === 1){
                 return dispatch({
                     type: END_GAME,
                     payload: {newState}
                 })
             }
-            // updates the state of projectiles
-            // updates position and distance traveled
+            // runs only if there are projectiles currently in play
             if(newState.projectiles.length !== 0){
                 // removes projectiles that have already traveled 4 spaces
                 const filteredProjectiles = newState.projectiles.filter(function(projectile){
@@ -48,11 +49,15 @@ export default function useGameLoop(state, dispatch){
                     }
                     return false
                 })
-
+                // maps through projectiles remaining after filtering and updates position and distance traveled
+                // -- if projectile collides with a wall and player, does damage to the wall or player
                 const updatedProjectiles = filteredProjectiles.map(projectile => {
+                    // creates a string of the name the current tile the projectile is in
                     const currentTileName = `x${projectile.x}y${projectile.y}`
+                    // gets the tile the projectile is currently in
                     const currentTile = newState.tileTracker[currentTileName]
                     const tileType = currentTile.type
+                    // decides what to do based on the type of tile the projectile is currently in
                     switch(tileType){
                         case EMPTY:
                             projectile.x = projectile.x+projectile.dx;
@@ -61,7 +66,10 @@ export default function useGameLoop(state, dispatch){
                             break
                         case WALL:
                             newState.tileTracker[currentTileName].health--
+                            // if wall has a health of 0, turns the tile into an empty tile
                             if(newState.tileTracker[currentTileName].health === 0){
+                                // updates tile to an empty tile now that it should no longer be a wall
+                                // spreads the current tile properties and adds the properties of the variable empty tile (see near top of code)
                                 newState.tileTracker[currentTileName] = {...newState.tileTracker[currentTileName], ...emptyTile}
                             }
                             projectile.distanceTraveled = GAME_PARAMS.PROJECTILE_RANGE;
@@ -79,6 +87,7 @@ export default function useGameLoop(state, dispatch){
                     }
                     return(projectile)
                 })
+                // sets newState projectiles to the updated projectiles
                 newState.projectiles = updatedProjectiles;
             }
             // =====================================
@@ -86,42 +95,50 @@ export default function useGameLoop(state, dispatch){
             newState.players = aiDecision(newState, dispatch)
             //=====================================================
              
-            // take action
+            // code below used to go through each player and decide whether they can take the action
+            // ... they are trying to perform and what are the outcomes of those actions
+            // -- also checks if the player is still alive
             const newPlayers = Object.entries(newState.players).map(([key, player]) => {
                 const name = player.name
+                // calculates players potential new position
                 const x = player.x+player.dx;
                 const y = player.y+player.dy;
+                // creates a string that represets the name of the current tile the player is in
                 const currentTileName = `x${player.x}y${player.y}`
+                // creates a string that represents the name of the potential tile the player will move into 
                 const tileToCheck = calcTileToCheck(player)
                 const newTileIsObstruction = newState.tileTracker[tileToCheck].isObstruction
                 // Check is player is still alive
                 if(player.health <= 0){
-                    // update tileTracker to indicate there is no longer anyone in the tyle
-                    newState.tileTracker[currentTileName].type = EMPTY;
-                    newState.tileTracker[currentTileName].isObstruction = false;
-                    newState.tileTracker[currentTileName].player = null;
-                    console.log(`${player.name} died`)
-                    console.log(player.name,newState.tileTracker[currentTileName])
+                    // update tileTracker to indicate there is no longer a player in the tile
+                    newState.tileTracker[currentTileName] = {...newState.tileTracker[currentTileName], ...emptyTile}
                     // set player isAlive to false
                     newState.players[name].isAlive = false;
+                    // make a copy of the player in outPlayers
                     newState.outPlayers[name] = newState.players[name]
+                    // delete the player from the players object so it can no longer make actions
                     delete newState.players[name]
+                    // this currently does nothing. It's just to stop VS Code from complaining that a map function doesn't have a return.
                     return("player")
                 }
+                // checks to see if player is trying to rotate
                 if(player.isRotating){
+                    // there really isn't any reason to not let the player rotate. If isRotating is true, currently player always rotates.
                     newState.players[name].orientation = player.newOrientation
                 }
+                // checks to see if player is trying to move and if the tile they are trying to move into is NOT an obstruction
                 if(player.isMoving && !newTileIsObstruction){
-                    // update player positions
+                    // update player position
                     newState.players[name].x = x;
                     newState.players[name].y = y;
                     // update tile moving into
                     newState.tileTracker[tileToCheck].isObstruction = true;
                     newState.tileTracker[tileToCheck].type = PLAYER;
                     newState.tileTracker[tileToCheck].player = player.name;
-                    // update tile moving out of
+                    // update tile moving out of to an empty tile
                     newState.tileTracker[currentTileName] = {...newState.tileTracker[currentTileName], ...emptyTile}
                 }
+                // checks to see if player is trying to build a wall and if the tile they are trying to build in is NOT an obstruction
                 if(player.isBuilding && !newTileIsObstruction){
                     newState.tileTracker[tileToCheck] = {
                         ...newState.tileTracker[tileToCheck],
@@ -130,6 +147,7 @@ export default function useGameLoop(state, dispatch){
                         health: GAME_PARAMS.MAX_WALL_HEALTH
                     }
                 }
+                // checks to see if player is trying to shoot. There is no reason for the player to be able to shoot. Action always occurs
                 if(player.isShooting){
                     const newProjectile = shootProjectile(player, tileToCheck)
                     newState.projectiles = [...newState.projectiles, newProjectile]
@@ -147,12 +165,15 @@ export default function useGameLoop(state, dispatch){
                 }
                 return (player)
             })
-         
+            // sends updated state to reducer to make state equal to newState
             return dispatch({
                 type: RERENDER,
                 payload: {newState}
             })
+        // sets the time for the timeout in milliseconds
         }, 333)
+        // clears the timeout so tons of timeouts do not start
         return () => clearTimeout(handleTime);
+        // tells the useEffect when it should occur
     }, [state.players, state.isRunning, state.projectiles])
 }
--- src/players/aiDecision.js
+++ src/players/aiDecision.js
@@ -1,33 +1,41 @@
-import { MOVE_PLAYER, BUILD_WALL, SHOOT_PROJECTILE} from "../state/actions"
-import { DIRECTIONS, GAME_PARAMS } from "../utils/constants"
+import { MOVE_PLAYER, BUILD_WALL, SHOOT_PROJECTILE } from "../state/actions"
+import { DIRECTIONS } from "../utils/constants"
 import moveAi from "../players/moveAi"
 
 export default function aiDecision(newState){
+    // creates a new object of the players to avoid mutating the object newState ... may not be necessary, but just in case
     const updatedPlayers = {...newState.players}
     const directions = [DIRECTIONS.NORTH, DIRECTIONS.SOUTH, DIRECTIONS.WEST, DIRECTIONS.EAST]
     
+    // used to select a random action for the ai to perform
     function selectRandomAction(){
-        // const actions = [MOVE_PLAYER, BUILD_WALL, SHOOT_PROJECTILE];
+        // all possible actions the ai can perform
         const actions = [MOVE_PLAYER, BUILD_WALL, SHOOT_PROJECTILE];
+        // calculate a random indice of the array actions
         const randomActionSelector = Math.floor(Math.random()*actions.length)
+        // use the random indice to get the random action
         const randomAction = actions[randomActionSelector]
         return(randomAction)
     }
    
-    // Object.entries(updatedPlayers).filter(function(player){
-    //     if(!player.isAlive){
-    //         return true
-    //     }
-    //     return false
-    // })
+    // loop through the object in state that contains all of the players
+    // Currently using map, but there is probably a better solution
     Object.entries(updatedPlayers).map(([key, player]) => {
+        // gets a player from the object. Since I am mapping through an object can't just use player, becaue player is just a property of the object updatedPlayers
         let updatedPlayer = updatedPlayers[player.name] 
+        // gets random action
         const action = selectRandomAction()
+        // run specific code based on random action
         switch(action){
             case MOVE_PLAYER:
+                // calculate a random indice of the array directions
                 const randomDirectionSelector = Math.floor(Math.random()*directions.length)
+                // use the random indice to get the random action
                 const randomDirection = directions[randomDirectionSelector];
+                // pass player name, random direction and newState to function moveAi
+                // -- moveAi decides wether the player should rotate or move based on current orientation and direction selected
                 updatedPlayer = moveAi(player.name, randomDirection, newState)
+                // updates player ... this is due to mapping through an object.
                 updatedPlayers[player.name] = updatedPlayer
                 return("player")
             case BUILD_WALL:
@@ -39,7 +47,7 @@ export default function aiDecision(newState){
             default:
                 console.log("silly computer, that isn't a move")
         }
-      
+        // Currently doesn't actually do anything. Just causes VS Code to stop complaining
         return("player")
     })
     return updatedPlayers
