dffb79793ec57c3f82fb6c8a2cee7eb951aded3a
SirPotatoIV <otoolej33@gmail.com>
2020-02-10 16:44:09 -0600

    fixed angry warning saying <a> can not be a decendent of <a>

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react"
-import {Anchor, Box, Button, Heading} from 'grommet';
-import {Home, Notification} from 'grommet-icons';
+import {Anchor, Box, Heading} from 'grommet';
+import {Home} from 'grommet-icons';
 import {Link} from 'react-router-dom'
 import SignedInLinks from "../SignedInLinks"
 import SignedOutLinks from "../SignedOutLinks"
@@ -18,7 +18,7 @@ function Navbar(props){
         style={{zIndex:'1'}}
         {...props}
     >
-        <Link to='/'><Anchor icon={<Home />} /></Link>
+        <Anchor icon={<Home />} label="Home" href="/" />
         <Heading level='3' margin='none'>Survive the Grid</Heading>
         <SignedInLinks/>
         <SignedOutLinks/>
