b90906948034576a099baca145a8dc28e6ed2e51
SirPotatoIV <otoolej33@gmail.com>
2020-02-11 19:07:55 -0600

    deleted files that are not being used. Started creating a home page.

--- src/components/Modal/Modal.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function Modal(){
-    return(
-    <div>
-        
-    </div>)
-}
-export default Modal;
--- src/utils/detectCollision.js
+++
@@ -1,7 +0,0 @@
-export default function detectCollision(state, x, y){
-    const targetTile = `x${x}y${y}`
-    if(state.tileTracker[targetTile].isObstruction){
-        return("obstruction")
-    }
-    return("not obscruction")
-}
